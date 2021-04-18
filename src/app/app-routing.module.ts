e8f6fae2ee71d137f908382bb7f600ff86eb6fd7
sujit <sujit.sharma@clusus.com>
2021-04-18 09:16:23 +0545

    Implement auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {AuthGuard} from './auth/auth-guard';
 
 const routes: Routes = [
   {
     path: '', component: HomeComponent
   },
   {
-    path: 'home', component: HomeComponent
+    path: 'home', component: HomeComponent, canActivate: [AuthGuard]
   },
   {
     path: 'auth', loadChildren: () => import('./login/auth.module').then((m) => m.AuthModule),
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -6,6 +6,8 @@ import {AuthResponse} from '../model/auth-response.model';
 import {environment} from '../../environments/environment';
 import {AppState} from '../shared/app.state';
 
+import jwt_decode from 'jwt-decode';
+
 
 @Injectable({
   providedIn: 'root',
@@ -23,5 +25,32 @@ export class AuthService {
 
   persistToken(token: string): void {
     localStorage.setItem('jwt_token', token);
+  }
+
+  isLoggedIn(): boolean {
+    const isValidToken  = this.isTokenValid();
+    return isValidToken;
+  }
+
+  private isTokenValid(): boolean {
+    const token = localStorage.getItem('jwt_token');
+    if (token) {
+      const decodedToken: object = this.jwtDecode(token);
+      const expiryTime: number = this.getTokenExpireTime(decodedToken);
+      return ((1000 * expiryTime) - (new Date()).getTime()) > 5000;
+    }
+    console.log('token does not exist');
+    return false;
+  }
+
+  private getTokenExpireTime(decodedToken: object): number {
+    // @ts-ignore
+    return decodedToken.exp;
+  }
+
+  private jwtDecode(token: string): object {
+    return jwt_decode(token);
+
+
   }
 }
