a5fc633cf1ccade198d05d08afeab6dee6b3baec
sujit <sujit.sharma@clusus.com>
2021-04-19 21:56:58 +0545

    jwt token interceptor implemented

---
+++ src/app/auth/auth.interceptor.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (this.needSkip(request.url)) {
+      console.log('skipped by Interceptor for ' + request.url);
+      return next.handle(request);
+    }
+    const authorizedRequest = request.clone( {
+      setHeaders: {
+        Authorization: `${localStorage.getItem('token')}`,
+      }
+    });
+    console.log('Authorization token added in request by Interceptor');
+    return next.handle(authorizedRequest);
+  }
+
+
+  private needSkip(url: string): boolean {
+    console.log('Checking Url to intercept request');
+    const positionToSkip = url.indexOf('/login');
+    return positionToSkip > 0;
+  }
+}
